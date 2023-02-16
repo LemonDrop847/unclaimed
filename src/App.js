0f5076bb6e2f3d3f58dbf73599e542e9eb807d5f
Anil Kumar Behera <ark845612@gmail.com>
2023-02-16 22:02:31 +0530

    dashboard css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home'
 import Navbar from './components/navbar';
 import Objects from './components/objectPage';
